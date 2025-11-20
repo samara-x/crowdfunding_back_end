cc29e70026aca3f625cd028427b5aa840a2917db
samara-x <samara_downey@hotmail.com>
2025-11-20 18:45:33 +1000

    add users and model relation fixes

--- crowdfunding/fundraisers/urls.py
+++ crowdfunding/fundraisers/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
-urlspatterns = [
+
+urlpatterns = [
     path('fundraisers/', views.FundraiserList.as_view()),
+    path('fundraisers/<int:pk>/', views.FundraiserDetail.as_view()),
+    path('pledges/', views.PledgeList.as_view())
 ]
