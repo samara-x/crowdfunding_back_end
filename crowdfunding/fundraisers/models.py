e612bc7641abe5bd32437aa505421bede23dce2d
samara-x <samara_downey@hotmail.com>
2026-02-28 12:39:40 +1000

    removed duplicate customer user model

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
-from django.db import timezone
-from datetime import timedelta
-
-
-# Create your models here.
-
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-
-    def __str__(self):
-        return self.username
     
 
 class Fundraiser(models.Model):
--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.username
