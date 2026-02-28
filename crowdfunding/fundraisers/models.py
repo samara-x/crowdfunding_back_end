14939e249b5caaaa6876f23d3fca969d9182ef10
samara-x <samara_downey@hotmail.com>
2026-02-28 12:16:30 +1000

    unknown

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.db import timezone
+from datetime import timedelta
+
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+    
 
 class Fundraiser(models.Model):
     title = models.CharField(max_length=200)
